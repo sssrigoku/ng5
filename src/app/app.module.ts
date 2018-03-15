488f6dc06fc8a1206b54e139926e2e7982a16e8a
sssrigoku <sssrigoku@gmail.com>
2018-03-15 12:23:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { Routes, Router, RouterModule } from '@angular/router'; 
+import { DataService } from './data.service';
 
-
-import { AppComponent } from './app.component';
-
-
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent} ,
+  { path: 'About/:id', component: AboutComponent} 
+]
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+HomeComponent,
+AboutComponent,
+],
+imports: [
+BrowserModule,
+FormsModule,
+BrowserAnimationsModule,
+RouterModule.forRoot(routes),
+],
+providers: [DataService],
+bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
